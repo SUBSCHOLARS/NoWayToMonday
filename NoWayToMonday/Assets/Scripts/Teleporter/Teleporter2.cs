30c7f9a0cd51b0ab61b18040b32ee19d33fedd77
Kota Hatanaka <kenkotan7722@gmail.com>
2025-06-24 21:37:00 +0900

    システム調整開始

--- NoWayToMonday/Assets/Scripts/Teleporter/Teleporter2.cs
+++ NoWayToMonday/Assets/Scripts/Teleporter/Teleporter2.cs
@@ -23,8 +23,8 @@ public class Teleporter2 : MonoBehaviour
     {
         if(other.gameObject.CompareTag("Player"))
         {
-            player.transform.position=new Vector3(52.6f,-7.2f,0f);
-            MainCamera.transform.position=new Vector3(62.5f,-0.19f,-10f);
+            player.transform.position=new Vector3(83f,-7.2f,0f);
+            MainCamera.transform.position=new Vector3(97.6f,17.3f,-10f);
             audioManager.PlayAuido();
             this.gameObject.SetActive(false);
             if(DayCountManager.DayCount!=7){

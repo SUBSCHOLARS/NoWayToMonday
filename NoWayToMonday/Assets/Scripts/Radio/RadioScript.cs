b843edc7f35226416af1d44c55c040ec95a86c1a
Kota Hatanaka <kenkotan7722@gmail.com>
2025-06-24 22:47:56 +0900

    何とかなってる（はず）

--- NoWayToMonday/Assets/Scripts/Radio/RadioScript.cs
+++ NoWayToMonday/Assets/Scripts/Radio/RadioScript.cs
@@ -4,17 +4,26 @@ using UnityEngine;
 
 public class RadioScript : MonoBehaviour
 {
-    [SerializeField]private AudioClip[] radioClips;
+    [SerializeField] private AudioClip[] radioClips;
     AudioSource audioSource;
     // Start is called before the first frame update
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
+        PlayRadio();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    public void PlayRadio()
+    {
+        audioSource.PlayOneShot(radioClips[DayCountManager.DayCount-1]);
+    }
+    public void StopRadio()
+    {
+        audioSource.Stop();
     }
 }

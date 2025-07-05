a683387f6618b341f6c4eedaf825fc2789457720
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-05 21:32:41 +0900

    どーしたもんか

--- NoWayToMonday/Assets/Scripts/AnimationState/AnimationStateChecker.cs
+++ NoWayToMonday/Assets/Scripts/AnimationState/AnimationStateChecker.cs
@@ -8,7 +8,7 @@ public class AnimationStateChecker : StateMachineBehaviour
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Debug.Log("アニメーションの再生が終了しました！");
-        if(animator.GetBool("Stabbing"))
+        if (animator.GetBool("Stabbing"))
         {
             Debug.Log("スタビングアニメーションが終了しました。");
             // ここでスタビングアニメーションが終了した後の処理を追加できます。

3c797b6ec785764ec050b437fc31c4830854d81f
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-05 22:10:03 +0900

    ここらで着地か...

--- NoWayToMonday/Assets/Scripts/AnimationState/AnimationStateChecker.cs
+++ NoWayToMonday/Assets/Scripts/AnimationState/AnimationStateChecker.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
+using UnityEngine.UI;
 
 public class AnimationStateChecker : StateMachineBehaviour
 {
+    public Image BlackOut;
     public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         Debug.Log("アニメーションの再生が終了しました！");
-        if (animator.GetBool("Stabbing"))
-        {
             Debug.Log("スタビングアニメーションが終了しました。");
             // ここでスタビングアニメーションが終了した後の処理を追加できます。
             // 例えば、次のシーンに遷移するなど。
-            SceneManager.LoadScene("KnifeBadEndText"); // 適切なシーン名に置き換えてください
-        }
+            SceneManager.LoadSceneAsync("KnifeBadEndText"); // 適切なシーン名に置き換えてください
 
+    }
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (animator.GetBool("Stabbing"))
+        {
+            BlackOut.gameObject.SetActive(true);
+            BlackOut.DOFade(1, 2.0f);
+            
+        }
     }
     // Start is called before the first frame update
     void Start()

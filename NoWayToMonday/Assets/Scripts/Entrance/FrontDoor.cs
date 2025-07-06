742c6fafd5eef09745e1679f195b8306a5a7bed2
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-07 00:46:35 +0900

    とりまこんなもんで

---
+++ NoWayToMonday/Assets/Scripts/Entrance/FrontDoor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Fungus;
+using UnityEngine;
+
+public class FrontDoor : MonoBehaviour
+{
+    private bool isOnDoor = false;
+    public Flowchart OpenFlowchat;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOnDoor && Input.GetKey(KeyCode.Space))
+        {
+            OpenFlowchat.ExecuteBlock("Open");
+        }
+    }
+    void OnCollisionStay2D(UnityEngine.Collision2D collision2D)
+    {
+        if (collision2D.gameObject.CompareTag("Player"))
+        {
+            isOnDoor = true;
+        }
+    }
+}

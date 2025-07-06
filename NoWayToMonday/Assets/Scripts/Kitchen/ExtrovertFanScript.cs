8baa06797c4711e0d6d353a7c75f0c1e39f7ec67
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-06 12:26:19 +0900

    んあ？

---
+++ NoWayToMonday/Assets/Scripts/Kitchen/ExtrovertFanScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtrovertFanScript : MonoBehaviour
+{
+    public GameObject ExtrovertFan;
+    Animator animator;
+    AudioSource audioSource;
+    bool isNearSwitch = false;
+    bool isFanStopped = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = ExtrovertFan.GetComponent<Animator>();
+        audioSource = ExtrovertFan.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isNearSwitch && !isFanStopped&& Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsStopFan", true);
+            audioSource.Pause();
+            isFanStopped = true;
+        }
+        else if (isNearSwitch && isFanStopped && Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("IsStopFan", false);
+            audioSource.UnPause();
+            isFanStopped = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isNearSwitch = true;
+            Debug.Log(isNearSwitch);
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isNearSwitch = false;
+            Debug.Log(isNearSwitch);
+        }
+    }
+}

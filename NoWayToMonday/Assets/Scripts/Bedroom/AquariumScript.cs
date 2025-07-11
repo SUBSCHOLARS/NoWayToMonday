d3b58b140c3fdaec214b6fa05a3c51b0c73d61a9
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-11 22:27:44 +0900

    うんうん

--- NoWayToMonday/Assets/Scripts/Bedroom/AquariumScript.cs
+++ NoWayToMonday/Assets/Scripts/Bedroom/AquariumScript.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Fungus;
 using UnityEngine;
 
 public class AquariumScript : MonoBehaviour
 {
+    bool isNearAquarium = false;
+    public Flowchart AquariumFlowchart;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,24 @@ public class AquariumScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SinkScript.isPodFilled && isNearAquarium && Input.GetKeyDown(KeyCode.Space))
+        {
+            AquariumFlowchart.ExecuteBlock("FillAquariumWithSand");
+            SinkScript.isPodFilled = false;
+        }
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isNearAquarium= true;
+        }
+    }
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isNearAquarium = false;
+        }
     }
 }

f97e4507162893800fae3df00afa491cc1dc471c
Kota Hatanaka <kenkotan7722@gmail.com>
2025-07-28 10:24:59 +0900

    うおあおい

--- NoWayToMonday/Assets/Scripts/Kanji/RefinedKanjiManager.cs
+++ NoWayToMonday/Assets/Scripts/Kanji/RefinedKanjiManager.cs
@@ -7,19 +7,31 @@ public class RefinedKanjiManager : MonoBehaviour
 {
     public Flowchart flowchart;
     private string randomKanji;
-    private string[] refinedKanjis = new string[]
+    private List<string> refinedKanjiList = new List<string>
     {
-        "衊", "寢", "蔽", "瞵", "錆", "靈", "蕐", "翳", "蠱","瀛"
+        "衊", "寢", "蔽", "瞵", "錆", "靈", "蕐", "翳", "蠱", "瀛"
     };
-    private string[] rareKanjis = new string[]
+    private List<string> rareKanjiList = new List<string>
     {
-        "彁","墸","妛","蟐"
+        "彁", "墸", "妛", "蟐"
     };
     public string GetRandomRefinedKanji()
     {
-        int randomIndex = Random.Range(0, refinedKanjis.Length);
-        randomKanji = refinedKanjis[randomIndex];
-        return randomKanji;
+        float weight = Random.Range(0f, 1f);
+        if (weight < 0.1f && rareKanjiList.Count > 0) // 10% chance to get a rare kanji
+        {
+            int randomIndex = Random.Range(0, rareKanjiList.Count);
+            randomKanji = rareKanjiList[randomIndex];
+            rareKanjiList.RemoveAt(randomIndex); // Remove to ensure uniqueness
+            return randomKanji;
+        }
+        else
+        {
+            int randomIndex = Random.Range(0, refinedKanjiList.Count);
+            randomKanji = refinedKanjiList[randomIndex];
+            refinedKanjiList.RemoveAt(randomIndex); // Remove to ensure uniqueness
+            return randomKanji;
+        }
     }
     public void SetRandomKanjiForSpecificSay()
     {

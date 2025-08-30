173e6bb0a608968b749cb6de490d60554f78a949
Kota Hatanaka <kenkotan7722@gmail.com>
2025-08-30 20:52:40 +0900

    おーいえあ

---
+++ NoWayToMonday/Assets/Scripts/Triggers/Curseday/LightOnAndOff.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class LightOnAndOff : MonoBehaviour
+{
+    public GameObject Light;
+    public Light2D globalLight;
+    public float dimmedIntensity = 0.3f;
+    private AudioSource audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        StartCoroutine(LightOnThenAndOff());
+        Debug.Log("Startメソッドが実行されました");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public IEnumerator LightOnThenAndOff()
+    {
+        while (true)
+        {
+            Debug.Log("ライトをオフにします");
+            Light.SetActive(false);
+            audioSource.PlayOneShot(audioSource.clip);
+            if (globalLight != null)
+            {
+                globalLight.intensity = dimmedIntensity;
+            }
+            yield return new WaitForSeconds(4.0f);
+
+            Debug.Log("ライトをオフにします");
+            Light.SetActive(true);
+            audioSource.PlayOneShot(audioSource.clip);
+            if (globalLight != null)
+            {
+                globalLight.intensity = 1f;
+            }
+            yield return new WaitForSeconds(4.0f);
+        }
+    }
+}
